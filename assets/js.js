d42e02b65143f782f76903578eac44e39cece46c
nadiabarbara <nadiabarbara10@gmail.com>
2025-03-26 16:22:00 +0200

    Update js.js

--- assets/js.js
+++ assets/js.js
@@ -14,9 +14,9 @@ let confirmDelete = confirm("Do you want to remove this item from the list?");
 // Check if the user clicked "OK" (true) or "Cancel" (false)
 if (confirmDelete) 
 { // If the user confirms, show a message that the item has been removed
-    alert("Item has been removed.");
+    console.log("Item has been removed.");
 } 
 else 
 {// If the user cancels, show a message that the item was not removed
-    alert("Item was not removed.");
+   console.log("Item was not removed.");
 }
